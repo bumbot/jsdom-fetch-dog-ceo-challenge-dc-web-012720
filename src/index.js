7bec14d92d46f5698d8035b3943e0d032186a322
willmharris <willmharris0@gmail.com>
2020-03-11 16:41:33 -0400

    finished challenges 1 and 2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 console.log('%c HI', 'color: firebrick')
+const imgURL = "https://dog.ceo/api/breeds/image/random/4"
+const breedURL = 'https://dog.ceo/api/breeds/list/all'
+const tableId = "dog-breeds"
+
+document.addEventListener("DOMContentLoaded", () => {
+    showImage()
+    showBreeds()
+})
+
+
+function showImage() {
+    fetch(imgURL).then(response => response.json()).then(imageArray =>
+        imageArray.message.forEach(url => putImageInDom(url))
+    )
+}
+
+function putImageInDom(url) {
+    let image = document.createElement('img')
+    image.src = url 
+    let ourDiv = document.querySelector("#dog-image-container")
+    ourDiv.append(image)
+}
+
+function showBreeds() {
+    fetch(breedURL).then(response => response.json()).then(breedArray => putBreedsInTable(breedArray) )
+}
+
+function putBreedsInTable (breedArray) {
+    for (const key in breedArray.message) {
+    let listItem = document.createElement('li') 
+    listItem.innerText = key
+    let list = document.querySelector(`#${tableId}`)
+    list.append(listItem)
+    }
+}
